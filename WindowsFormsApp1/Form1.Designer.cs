3ff6472d91fbbb22f32a785fb32b0edb80b7cc8c
Arlopol <125224000+Arlopol@users.noreply.github.com>
2024-05-09 16:09:05 +0300

    update

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -32,6 +32,7 @@ namespace WindowsFormsApp1
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // button1
@@ -52,6 +53,7 @@ namespace WindowsFormsApp1
             this.button2.TabIndex = 1;
             this.button2.Text = "Inner Join";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // listBox1
             // 
@@ -61,16 +63,27 @@ namespace WindowsFormsApp1
             this.listBox1.Size = new System.Drawing.Size(172, 134);
             this.listBox1.TabIndex = 2;
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(545, 90);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
@@ -80,6 +93,7 @@ namespace WindowsFormsApp1
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -32,17 +32,14 @@ namespace WindowsFormsApp1
                 int job_id = person_Read.GetInt32(2);
                 int car_id = Convert.ToInt32(person_Read[3]);
 
-                OleDbCommand job_Command = new OleDbCommand("select meslek_adı from meslek where meslek_id=" +job_id, connection);
-
+                OleDbCommand job_Command = new OleDbCommand("select meslek_adı from meslek where meslek_id=" + job_id, connection);
                 OleDbDataReader job_Read = job_Command.ExecuteReader();
                 job_Read.Read();
-
                 string job_name = job_Read["meslek_adı"].ToString();
 
                 OleDbCommand car_command = new OleDbCommand("select araba_adı from araba where araba_id=" + car_id, connection);
                 OleDbDataReader car_read = car_command.ExecuteReader();
                 car_read.Read();
-
                 string car_name = car_read["araba_adı"].ToString();
 
                 listBox1.Items.Add(name + "\t" + job_name + "\t" + car_name);
@@ -50,6 +47,48 @@ namespace WindowsFormsApp1
 
             }
 
+            connection.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            OleDbCommand inner_Command = new OleDbCommand("SELECT kişi.adı, meslek.meslek_adı, araba.araba_adı FROM meslek INNER JOIN (araba INNER JOIN kişi ON araba.araba_id = kişi.araba_id) ON meslek.meslek_id = kişi.meslek_id;", connection);
+            OleDbDataReader inner_Read = inner_Command.ExecuteReader();
+
+            while (inner_Read.Read())
+            {
+                listBox1.Items.Add(inner_Read["adı"] + "\t" + inner_Read["meslek_adı"] + "\t" + inner_Read["araba_adı"]);
+            }
+
+
+            connection.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connection.Open();
+            OleDbCommand combo_command = new OleDbCommand("select kişi.adı from kişi", connection);
+            OleDbDataReader combo_read = combo_command.ExecuteReader();
+            while (combo_read.Read())
+            {
+                comboBox1.Items.Add(combo_read["adı"]);
+            }
+            connection.Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            connection.Open();
+            OleDbCommand inner_command = new OleDbCommand("SELECT meslek.meslek_adı, araba.araba_adı, kişi.adı FROM meslek INNER JOIN (araba INNER JOIN kişi ON araba.araba_id = kişi.araba_id) ON meslek.meslek_id = kişi.meslek_id WHERE (((kişi.adı)='"+comboBox1.SelectedItem.ToString()+"' ))", connection);
+            OleDbDataReader inner_Read = inner_command.ExecuteReader();
+
+            while (inner_Read.Read())
+            {
+                listBox1.Items.Add(inner_Read["adı"] + "\t" + inner_Read["meslek_adı"] + "\t" + inner_Read["araba_adı"]);
+                
+
+            }
             connection.Close();
         }
     }
